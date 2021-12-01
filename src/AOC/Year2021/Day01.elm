b15e95cf5a9c0cc2d4599de6bdf96677a12232cc
Nate Brunette <nate.brunette@drip.com>
2021-12-01 11:43:04 -0600

    day 1, 2021

---
+++ src/AOC/Year2021/Day01.elm
@@ -0,0 +1,31 @@
+module AOC.Year2021.Day01 exposing (part1, part2)
+
+import List.Extra
+import Shared.ListExt as ListExt
+
+
+part1 : List String -> String
+part1 input =
+    input
+        |> ListExt.toIntList
+        |> calculateIncreasing
+        |> String.fromInt
+
+
+part2 : List String -> String
+part2 input =
+    input
+        |> ListExt.toIntList
+        |> List.Extra.groupsOfWithStep 3 1
+        |> List.map List.sum
+        |> calculateIncreasing
+        |> String.fromInt
+
+
+calculateIncreasing : List Int -> Int
+calculateIncreasing list =
+    let
+        offsets =
+            List.tail list |> Maybe.withDefault []
+    in
+    List.Extra.zip list offsets |> List.Extra.count (\( a, b ) -> b > a)
